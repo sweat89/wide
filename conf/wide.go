13ebd0ce8e84e1c6b6bb9d9ad4c91feb9a17f894
Liang Ding <DL88250@gmail.com>
2014-08-06 23:01:52 +0800

    .

---
+++ conf/wide.go
@@ -0,0 +1,45 @@
+package conf
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/golang/glog"
+	"io/ioutil"
+)
+
+type Conf struct {
+	ServerPort            string
+	StaticServerScheme    string
+	StaticServerHost      string
+	StaticServerPort      string
+	StaticResourceVersion string
+	ContextPath           string
+	StaticPath            string
+	RuntimeMode           string
+	ProjectHome           string
+
+	StaticServer string
+}
+
+var Wide Conf
+
+func init() {
+	flag.Set("logtostderr", "true")
+
+	flag.Parse()
+
+	bytes, _ := ioutil.ReadFile("conf/wide.json")
+
+	err := json.Unmarshal(bytes, &Wide)
+	if err != nil {
+		glog.Errorln(err)
+		return
+	}
+
+	Wide.StaticServer = Wide.StaticServerScheme +
+		"://" + Wide.StaticServerHost +
+		":" + Wide.StaticServerPort +
+		Wide.StaticPath
+
+	glog.Info("Conf: \n" + string(bytes))
+}
---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/88250/wide/conf"
+	"github.com/golang/glog"
+	"github.com/gorilla/websocket"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+const PATH_SEPARATOR = string(os.PathSeparator)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+
+	if nil != err {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	t.Execute(w, map[string]string{"StaticServer": conf.Wide.StaticServer})
+}
+
+func run(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var c interface{}
+
+	if err := decoder.Decode(&c); err != nil {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	m := c.(map[string]interface{})
+
+	projectName := m["project"].(string)
+	projectPath := conf.Wide.ProjectHome + PATH_SEPARATOR + projectName
+	filePath := projectPath + PATH_SEPARATOR + m["file"].(string)
+
+	fout, err := os.Create(filePath)
+
+	if nil != err {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	fout.WriteString(m["code"].(string))
+
+	if err := fout.Close(); nil != err {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	argv := []string{"run", filePath}
+
+	cmd := exec.Command("go", argv...)
+
+	stdout, err := cmd.StdoutPipe()
+	if nil != err {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if nil != err {
+		glog.Error(err)
+		http.Error(w, err.Error(), 500)
+
+		return
+	}
+
+	reader := io.MultiReader(stdout, stderr)
+
+	cmd.Start()
+
+	rec := map[string]interface{}{}
+
+	for {
+		buf := make([]byte, 1024)
+		count, err := reader.Read(buf)
+		if nil != err || 0 == count {
+			break
+		} else {
+			rec["output"] = string(buf)
+
+			err := outputWS.WriteJSON(&rec)
+			if nil != err {
+				glog.Error(err)
+				break
+			}
+		}
+	}
+
+	ret, _ := json.Marshal(map[string]string{"succ": "true"})
+
+	w.Write(ret)
+}
+
+// TODO: 多会话支持
+var outputWS *websocket.Conn
+
+func output(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		glog.Error(err)
+		return
+	}
+
+	outputWS = ws
+
+	ret := map[string]interface{}{"output": "Ouput 初始化完毕\n"}
+	outputWS.WriteJSON(&ret)
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", index)
+	http.HandleFunc("/run", run)
+
+	http.HandleFunc("/output", output)
+
+	err := http.ListenAndServe(":"+conf.Wide.ServerPort, nil)
+	if err != nil {
+		glog.Fatal(err)
+	}
+}
